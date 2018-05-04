37382983051a17aef0c3c8f2d644aeeee7b327f6
Long <longtd3012@gmail.com>
2018-05-04 16:42:01 +0700

    hash password

--- server/server.js
+++ server/server.js
@@ -15,6 +15,9 @@ var {
     User
 } = require('./models/user');
 
+var {
+    authenticate
+} = require('./middleware/authenticate');
 var app = express();
 const port = process.env.PORT || 3000;
 
@@ -133,6 +136,11 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     });
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 
